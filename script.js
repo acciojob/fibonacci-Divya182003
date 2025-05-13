48ecbe5fb3eebf23fee885834cdd88d7a1d5c9ad
Divya182003 <divyabhootda05@gmail.com>
2025-05-13 14:09:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
 function fibonacci(num) {
-  if (num <= 0 || num > 50) return null; // Handle invalid input
-
-  if (num === 1) return 0;
-  if (num === 2) return 1;
-
-  let a = 0, b = 1;
-  for (let i = 3; i <= num; i++) {
-    let temp = a + b;
-    a = b;
-    b = temp;
+  if (num <= 0) {
+    return 0;
+  } else if (num === 1) {
+    return 0;
+  } else if (num === 2) {
+    return 1;
+  } else {
+    let a = 0;
+    let b = 1;
+    for (let i = 3; i <= num; i++) {
+      let c = a + b;
+      a = b;
+      b = c;
+    }
+    return b;
   }
-
-  return b;
 }
 
-// Example test cases:
-console.log(fibonacci(1));  // Output: 0
-console.log(fibonacci(5));  // Output: 3
-console.log(fibonacci(10)); // Output: 34
 
 
