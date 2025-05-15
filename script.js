fe5293ddd8d79656fe585d3f5e12037d4f0ba866
Divya182003 <divyabhootda05@gmail.com>
2025-05-15 10:36:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
 function fibonacci(num) {
-    if (num === 1) return 0;
-    if (num === 2) return 1;
-
-    let a = 0, b = 1, fib;
-
-    for (let i = 3; i <= num; i++) {
-        fib = a + b;
-        a = b;
-        b = fib;
+  /**
+   * Finds the nth Fibonacci number.
+   *
+   * Args:
+   * num: A positive integer representing the index of the desired Fibonacci number (0-based).
+   *
+   * Returns:
+   * The nth Fibonacci number.
+   */
+  if (num < 0) {
+    throw new Error("Input must be a non-negative integer.");
+  } else if (num <= 1) {
+    return num;
+  } else {
+    let a = 0;
+    let b = 1;
+    for (let i = 2; i <= num; i++) {
+      let c = a + b;
+      a = b;
+      b = c;
     }
-
-    return fib;
+    return b;
+  }
 }
 
 // Example usage:
-console.log(fibonacci(1)); // Output: 0
-console.log(fibonacci(2)); // Output: 1
-console.log(fibonacci(6)); // Output: 5
-console.log(fibonacci(10)); // Output: 34
+console.log(`The 0th Fibonacci number is: ${fibonacci(0)}`);
+console.log(`The 1st Fibonacci number is: ${fibonacci(1)}`);
+console.log(`The 2nd Fibonacci number is: ${fibonacci(2)}`);
+console.log(`The 3rd Fibonacci number is: ${fibonacci(3)}`);
+console.log(`The 10th Fibonacci number is: ${fibonacci(10)}`);
+console.log(`The 20th Fibonacci number is: ${fibonacci(20)}`);
