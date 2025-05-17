8673f70ffc45045ab5c8a96019463dabc24d18bd
Divya182003 <divyabhootda05@gmail.com>
2025-05-17 13:35:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-fibanocii    function fibonacci(num) {
+function fibonacci(num) {
     if (num === 0) return 0;
-    if (num === 1) return 0;
-    if (num === 2) return 1;
+    if (num === 1) return 1;
 
-    let a = 0, b = 1, c;
-    for (let i = 3; i <= num; i++) {
-        c = a + b;
+    let a = 0, b = 1;
+    for (let i = 2; i <= num; i++) {
+        let temp = a + b;
         a = b;
-        b = c;
+        b = temp;
     }
     return b;
 }
 
-module.exports = fibonacci;
+// Example usage:
+console.log(fibonacci(0));  // Output: 0
+console.log(fibonacci(1));  // Output: 1
+console.log(fibonacci(5));  // Output: 5
+console.log(fibonacci(10)); // Output: 55
+console.log(fibonacci(50)); // Output: 12586269025
